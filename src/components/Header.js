96719476cd3c0c5265a93b86b3205cbcc956b9af
James Ford <psyked84@gmail.com>
2019-05-27 16:03:25 +0100

    Add a new section for speaking

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,33 @@ import Link from 'gatsby-link'
 
 import styles from './header.module.scss'
 
-const Header = (props) => (
-    <header className={styles.root}>
-        <nav className="bodyContent">
-            <Link to="/" className={styles.menuLink} activeClassName={styles.active} exact>Home</Link>
-            <Link to="/blog/" className={styles.menuLink} activeClassName={styles.active}>Blog Archive</Link>
-        </nav>
-    </header>
+const Header = props => (
+  <header className={styles.root}>
+    <nav className="bodyContent">
+      <Link
+        to="/"
+        className={styles.menuLink}
+        activeClassName={styles.active}
+        exact
+      >
+        Home
+      </Link>
+      <Link
+        to="/blog/"
+        className={styles.menuLink}
+        activeClassName={styles.active}
+      >
+        Blog Archive
+      </Link>
+      <Link
+        to="/speaking/"
+        className={styles.menuLink}
+        activeClassName={styles.active}
+      >
+        Public Speaking
+      </Link>
+    </nav>
+  </header>
 )
 
 export default Header
