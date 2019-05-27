fd3f0d5e881865e09a64930f0230316d3c48796e
James Ford <psyked84@gmail.com>
2019-05-27 16:03:45 +0100

    Add new pages for speaking gigs

---
+++ src/pages/speaking.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+class Speaking extends Component {
+  render() {
+    const {
+      data: {
+        allMarkdownRemark: { edges: presentations },
+        site: {
+          siteMetadata: { title: siteTitle, description: siteDescription },
+        },
+      },
+    } = this.props
+
+    return (
+      <Layout>
+        <div className="bodyContent">
+          <Helmet
+            htmlAttributes={{
+              lang: 'en',
+            }}
+          >
+            <title>{siteTitle}</title>
+            <meta name="description" content={siteDescription} />
+          </Helmet>
+
+          <header className="major">
+            <h2>Public Speaking</h2>
+          </header>
+
+          {presentations.map(({ node: presentation }) => {
+            return (
+              <>
+                <section>
+                  <h3>{presentation.frontmatter.title}</h3>
+                  {presentation.excerpt}
+                </section>
+                <hr />
+              </>
+            )
+          })}
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Speaking
+
+export const pageQuery = graphql`
+  query SpeakingQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(
+      filter: { frontmatter: { layout: { eq: "presentation" } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          html
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/presentations/make-your-tools-work.md
@@ -0,0 +1,8 @@
+---
+path: 'speaking/make-your-tools-work'
+layout: 'presentation'
+title: 'Make Your Tools Work'
+description: 'Specifically, make your tools work for you.'
+---
+
+content coming soon!
