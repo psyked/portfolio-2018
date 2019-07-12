e90fba2dd9f35d97bd272afdfe4ca41e17108fda
James Ford <psyked84@gmail.com>
2019-07-12 21:56:17 +0100

    add basic dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,41 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
+import ThemeSwitch from './ThemeSwitch'
+
 const Header = styled.header`
   max-width: 60em;
   margin: 0 auto;
   position: sticky;
   z-index: 100;
   display: block;
-  margin-bottom: -60px;
+  margin-bottom: -68px;
+  padding-top: 0.5em;
 `
 
 const Nav = styled.nav`
   border-radius: 0.3em;
   background: hsl(240, 14%, 19%);
-  border: 1px solid hsl(240, 14%, 99%);
   padding: 0.5em 0.2em 0.5em 0.7em;
   top: 0;
   display: inline-block;
-  margin-top: 0.5em;
-  margin-left: -2em;
 `
 
 const MenuLink = styled(Link)`
   margin-right: 0.5em;
-  color: hsl(240, 14%, 99%);
   padding: 0.4em;
   margin-left: -0.4em;
-  position: relative;
+
+  &,
+  a {
+    &,
+    :link,
+    :active,
+    :visited,
+    :hover {
+      color: hsl(240, 14%, 99%);
+    }
+  }
 
   &:last-child {
     margin-right: 0;
@@ -39,19 +48,29 @@ const MenuLink = styled(Link)`
   }
 `
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1em;
+`
+
 const HeaderComponent = props => (
   <Header>
-    <Nav>
-      <MenuLink to="/" activeClassName="active" exact>
-        Home
-      </MenuLink>
-      <MenuLink to="/blog/" activeClassName="active">
-        Blog Archive
-      </MenuLink>
-      <MenuLink to="/speaking/" activeClassName="active">
-        Public Speaking
-      </MenuLink>
-    </Nav>
+    <Wrapper>
+      <Nav>
+        <MenuLink to="/" activeClassName="active" exact>
+          Home
+        </MenuLink>
+        <MenuLink to="/blog/" activeClassName="active">
+          Blog Archive
+        </MenuLink>
+        <MenuLink to="/speaking/" activeClassName="active">
+          Public Speaking
+        </MenuLink>
+      </Nav>
+      <ThemeSwitch />
+    </Wrapper>
   </Header>
 )
 
---
+++ src/components/ThemeSwitch.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import styled from 'styled-components'
+
+const SwitchWrapper = styled.label`
+  background-color: #ddd;
+  border-radius: 18px;
+  cursor: pointer;
+  display: inline-block;
+  height: 20px;
+  padding: 3px;
+  position: relative;
+  vertical-align: top;
+  width: 56px;
+  box-sizing: content-box;
+`
+
+const Label = styled.span`
+  background: #ffd700 none repeat scroll 0 0;
+  border-radius: inherit;
+  display: block;
+  font-size: 10px;
+  height: inherit;
+  position: relative;
+  text-transform: uppercase;
+  transition: all 0.15s ease-out 0s;
+
+  ::before,
+  ::after {
+    line-height: 1;
+    margin-top: -0.5em;
+    position: absolute;
+    top: 50%;
+    transition: inherit;
+  }
+`
+
+const Handle = styled.span`
+  background-color: #fff;
+  border-radius: 10px;
+  height: 18px;
+  left: 4px;
+  position: absolute;
+  top: 4px;
+  transition: left 0.15s ease-out 0s;
+  width: 18px;
+
+  ::before {
+    background-color: #f9f9f9;
+    border-radius: 6px;
+    content: '';
+    height: 12px;
+    left: 50%;
+    margin: -6px 0 0 -6px;
+    position: absolute;
+    top: 50%;
+    width: 12px;
+  }
+`
+
+const Input = styled.input`
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+
+  :checked ~ ${Label} {
+    background: #eceeef none repeat scroll 0 0;
+  }
+
+  :checked ~ ${Handle} {
+    left: 40px;
+  }
+`
+
+class ThemeSwitch extends React.Component {
+  render() {
+    return (
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <SwitchWrapper class="switch">
+            <Input
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+            />
+            <Label />
+            <Handle />
+          </SwitchWrapper>
+        )}
+      </ThemeToggler>
+    )
+  }
+}
+
+export default ThemeSwitch
