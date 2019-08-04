b4e403d4b8ffc4eaa949b711fbbddd7957d3597f
James Ford <psyked84@gmail.com>
2019-08-04 11:58:19 +0100

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 1200,
             },
           },
           'gatsby-remark-copy-linked-files',
