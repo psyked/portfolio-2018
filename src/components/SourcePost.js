6528886e2f858bcf130fe97cfe1339227d201d50
James Ford <psyked84@gmail.com>
2019-11-03 19:57:37 +0000

    refactor to stlyed components

--- src/components/SourcePost.js
+++ src/components/SourcePost.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-import styles from './sourcepost.module.scss'
+const StyledElement = styled.aside`
+  margin: 1em -2em;
+  padding: 1em 2em;
+  background: linear-gradient(90deg,#d0d0dc 0,#d0d0dc 8px,#eeeef2 0);
+
+  .dark & {
+    background: linear-gradient(90deg,#3a3a48 0,#3a3a48 8px,#3d3d4a 0);
+  }
+`
 
 const SourcePost = ({ url }) => {
-    if (!url) return <aside />
-    return (
-        <aside className={styles.root}>
-            This article was originally published at <a href={url}>{url}</a>
-        </aside>
-    )
+  if (!url) return <Fragment />
+  return (
+    <StyledElement>
+      This article was originally published at <a href={url}>{url}</a>
+    </StyledElement>
+  )
 }
 
-SourcePost.propTypes = {
-
-}
+SourcePost.propTypes = {}
 
 export default SourcePost
